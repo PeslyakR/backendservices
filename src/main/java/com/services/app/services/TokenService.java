f1a8ec6b4b9f7bbf609de4aef8cf8573778bc7cd
PeslyakR <replicatorexe1@gmail.com>
2022-01-27 22:14:00 +0200

    deleted some trash

--- src/main/java/com/services/app/services/TokenService.java
+++ src/main/java/com/services/app/services/TokenService.java
@@ -15,12 +15,10 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 
 @Service
 @RequiredArgsConstructor
 @Transactional
-@Slf4j
 public class TokenService implements UserDetailsService {
 
     private final IUserRepository userRepo;
@@ -29,12 +27,9 @@ public class TokenService implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         UserEntity user = userRepo.findByUsername(username);
-        System.out.println("-------------(((((" + username);
         if (user == null) {
-            log.error("User not found");
             throw new UsernameNotFoundException("UserNotFound!");
         } else {
-            log.info("User vas found");
         }
         Collection<SimpleGrantedAuthority> authorities = new ArrayList<>();
         user.getEmployee().getRoles()
@@ -47,7 +42,6 @@ public class TokenService implements UserDetailsService {
     }
 
     public UserEntity getUser(String username) {
-        log.info("Fetching user");
         return userRepo.findByUsername(username);
     }
 
